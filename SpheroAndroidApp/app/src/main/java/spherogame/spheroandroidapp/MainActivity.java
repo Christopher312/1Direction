7e87787faaca8a49cdf35b1e39432dd1e52e457e
christopher morris <christopher.morris55@gmail.com>
2016-10-29 13:07:59 -0400

    instructions

--- SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MainActivity.java
+++ SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MainActivity.java
@@ -2,6 +2,8 @@ package spherogame.spheroandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.content.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        //System.out.println("starting game");
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewInstructions(View view) {
+        Intent intent = new Intent(this, InstructionsActivity.class);
+        startActivity(intent);
     }
 }
