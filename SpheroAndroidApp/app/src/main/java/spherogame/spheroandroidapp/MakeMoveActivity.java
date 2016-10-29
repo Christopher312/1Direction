e254c1c97f5fc8e5499c83301b9971933210009c
christopher morris <christopher.morris55@gmail.com>
2016-10-29 14:57:01 -0400

    making moves

--- SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MakeMoveActivity.java
+++ SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MakeMoveActivity.java
@@ -2,6 +2,10 @@ package spherogame.spheroandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.math.*;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
 
 public class MakeMoveActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MakeMoveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_move);
+        int card = (int) (Math.random()*6);
+        TextView cardText = (TextView) findViewById(R.id.textView5);
+        cardText.setText("Your card:\n        " + card);
+        //TODO: retrieve data
+    }
+
+    public void FinishTurn(View view) {
+        int angle = Integer.parseInt(((EditText) findViewById(R.id.editText)).getText().toString());
+        int distance = Integer.parseInt(((EditText) findViewById(R.id.editText2)).getText().toString());
+        Intent intent = new Intent(this, PassPhoneActivity.class);
+        startActivity(intent);
+        //TODO: Send data
     }
 }
