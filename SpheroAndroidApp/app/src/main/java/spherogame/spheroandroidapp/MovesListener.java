b46121739fdee935529c09539c7ab682f3fcf858
Grace <graceyyu@gmail.com>
2016-10-29 19:53:20 -0400

    moveslistener

--- SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MovesListener.java
+++ SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MovesListener.java
@@ -10,24 +10,23 @@ import com.bezirk.middleware.messages.EventSet;
 public class MovesListener {
     public MovesListener() {
         BezirkMiddleware.initialize(this, "macaronipenguins"); 
-        final Bezirk bezirk = BezirkMiddleware.registerZirk("Moves Listener");  
+        final Bezirk bezirk = BezirkMiddleware.registerZirk("macaronipenguins");  
         EventSet turnEvents = new EventSet(MovementInstructionEvent.class); 
         turnEvents.setEventReceiver(new EventSet.EventReceiver()) { 
             @Override 
             public void receiveEvent(Event event, ZirkEndPoint sender) { 
                 if (event instanceof MovementInstructionEvent) { 
-                    final MovementInstructionEvent currentTurn = (MovementInstructionEvent) event; 
+                    MovementInstructionEvent currentTurn = (MovementInstructionEvent) event; 
                     double[][] moves = new double[currentTurn.angles.length][2];
                     for (int i = 0; i < currentTurn.angles.length; i++) {
                         moves[i][0] = currentTurn.angles[i];
                         moves[i][1] = currentTurn.distances[i];
                     }
-                    System.out.println("\nReceived turn: " + currentTurn.toString()); 
-                    // do something 
                     TurnEvent turn = new TurnEvent(moves);
                     bezirk.sendEvent(turn);
                 } 
             } 
         }
+        bezirk.subscribe(eventSet);
     }
 }
