e58727691b9bc776ca267a4bff421040a8eed0fa
Marcus <marcusa3.14@gmail.com>
2016-10-29 15:28:11 -0400

    makemove

--- SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MakeMoveActivity.java
+++ SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MakeMoveActivity.java
@@ -20,7 +20,7 @@ public class MakeMoveActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int players = intent.getIntExtra("players", 4);
         int rounds = intent.getIntExtra("rounds", 5)-1;
-        TextView TV1 = (TextView) findViewById(R.id.MMround);
+        TextView TV1 = (TextView) findViewById(R.id.MMrounds);
         TV1.setText("Round "+rounds);
         TextView TV2 = (TextView) findViewById(R.id.MMplayers);
         TV2.setText("Players"+players);
@@ -29,10 +29,27 @@ public class MakeMoveActivity extends AppCompatActivity {
     public void FinishTurn(View view) {
         int angle = Integer.parseInt(((EditText) findViewById(R.id.editText)).getText().toString());
         int distance = Integer.parseInt(((EditText) findViewById(R.id.editText2)).getText().toString());
-        Intent intent = new Intent(this, PassPhoneActivity.class);
-        startActivity(intent);
-        TextView TV1 = (TextView) findViewById(R.id.MMround);
+        TextView TV1 = (TextView) findViewById(R.id.MMrounds);
         int rounds = Integer.parseInt(TV1.getText().toString());
-        //TODO: Send data
+        TextView TV2 = (TextView) findViewById(R.id.MMplayers);
+        int players = Integer.parseInt(TV1.getText().toString());
+        if(rounds==0)
+        {
+            Intent intent = new Intent(this, MakeTurnActivity.class);
+            intent.putExtra("angle", angle);
+            intent.putExtra("distance", distance);
+            intent.putExtra("rounds", rounds);
+            intent.putExtra("players", players);
+            startActivity(intent);
+        }
+        else
+        {
+            Intent intent = new Intent(this, PassPhoneActivity.class);
+            intent.putExtra("angle", angle);
+            intent.putExtra("distance", distance);
+            intent.putExtra("rounds", rounds);
+            intent.putExtra("players", players);
+            startActivity(intent);
+        }
     }
 }
