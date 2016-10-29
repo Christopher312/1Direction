1921b05c43aefda2764bd150f4e5dbca2bc22377
christopher morris <christopher.morris55@gmail.com>
2016-10-29 15:28:56 -0400

    code

--- SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MakeTurnActivity.java
+++ SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/MakeTurnActivity.java
@@ -2,6 +2,8 @@ package spherogame.spheroandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.content.*;
 
 public class MakeTurnActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MakeTurnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_turn);
+    }
+
+    public void makeTurn(View view) {
+        //TODO: SEND COMMANDS TO THE F*ING ROBOT
+        //TODO: test for victory conditions
+        //TODO: send data to the MakeMove
+        Intent intent = new Intent(this, MakeMoveActivity.class);
+        startActivity(intent);
     }
 }
--- SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/PassPhoneActivity.java
+++ SpheroAndroidApp/app/src/main/java/spherogame/spheroandroidapp/PassPhoneActivity.java
@@ -2,6 +2,8 @@ package spherogame.spheroandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+import android.content.*;
 
 public class PassPhoneActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class PassPhoneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_phone);
+    }
+
+    public void passPhone(View view) {
+        Intent intent = new Intent(this, MakeMoveActivity.class);
+        startActivity(intent);
+        //TODO: relay the data
     }
 }
